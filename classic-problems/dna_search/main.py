8c413d7a6f4f36b259bafd71639e72027a69186d
igaozp <igaozp@hotmail.com>
2023-06-06 20:29:58 +0800

    feat: improve code

--- classic-problems/dna_search/main.py
+++ classic-problems/dna_search/main.py
@@ -1,7 +1,7 @@
 from enum import IntEnum
 from typing import Tuple, List
 
-Nucleotide: IntEnum = IntEnum('Nucleotide', ('A', 'C', 'G', 'T'))
+Nucleotide = IntEnum('Nucleotide', ('A', 'C', 'G', 'T'))
 Codon = Tuple[Nucleotide, Nucleotide, Nucleotide]
 Gene = List[Codon]
 
